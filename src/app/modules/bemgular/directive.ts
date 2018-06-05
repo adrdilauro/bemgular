046f388cb460b7c87b44c4c702c0e3e0183c1554
Adriano <adriano.dilauro@boatinternationalmedia.com>
2018-06-05 17:40:53 +0100

    Final refinements

--- src/app/modules/bemgular/directive.ts
+++ src/app/modules/bemgular/directive.ts
@@ -5,6 +5,7 @@ import { BEMGULAR_BLOCK, BEMGULAR_MODIFIERS } from './tokens';
   selector: '[bem]',
 })
 export class BemgularDirective implements OnInit {
+  private _EMPTY_STRING: string = '';
   private _block: string;
   private _modifiers: string[];
   private _parentBlock: string;
@@ -16,36 +17,34 @@ export class BemgularDirective implements OnInit {
     @SkipSelf() private _parentInjector: Injector,
     private _elRef: ElementRef
   ) {
-    this._block = this._injector.get(BEMGULAR_BLOCK, '');
+    this._block = this._injector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
     this._modifiers = this._injector.get(BEMGULAR_MODIFIERS, []);
-    this._parentBlock = this._parentInjector.get(BEMGULAR_BLOCK, '');
+    this._parentBlock = this._parentInjector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
     this._parentModifiers = this._parentInjector.get(BEMGULAR_MODIFIERS, []);
   }
 
   @Input('bem')
   set storeElementAndModifiers(value: string) {
     let split = value.split(',');
-    let element: string = (!!split[0]) ? split[0].trim() : 'block';
+    let element: string = (!!split[0]) ? split[0].trim() : this._EMPTY_STRING;
     let elementModifiers: string[] = split.slice(1).map(modifier => modifier.trim());
     if (this._block !== this._parentBlock) {
-      let valueArray: string[] = [];
-      if (this._parentBlock !== '') {
-        valueArray.push(this.bemElement(this._parentBlock, this._parentModifiers, element, elementModifiers));
-      }
-      if (this._block !== '') {
-        valueArray.push(this.bemBlockArray(this._block, this._modifiers).join(' '));
-      }
-      this._value = valueArray.join(' ');
-    } else if (this._block !== '') {
+      this._block = this._parentBlock;
+      this._modifiers = this._parentModifiers;
+    }
+    if (this._block !== this._EMPTY_STRING) {
       this._value = this.bemElement(this._block, this._modifiers, element, elementModifiers);
     }
   }
 
   ngOnInit() {
-    this._elRef.nativeElement.className = this._value;
+    this._elRef.nativeElement.className = this._value.trim();
   }
 
   private bemElement(block, modifiers, element, elementModifiers) {
+    if (element === this._EMPTY_STRING) {
+      return '';
+    }
     return this.bemBlockArray(block, modifiers).map(blockWithModifiers => {
       return this.bemBlockArray(element, elementModifiers).map(elementWithModifiers => {
         return blockWithModifiers + '__' + elementWithModifiers;
