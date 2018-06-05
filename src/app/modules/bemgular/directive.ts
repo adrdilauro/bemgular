39bc364c75ea83324e980cf6c7db97e4c308c256
Adriano <adriano.dilauro@boatinternationalmedia.com>
2018-06-05 17:47:11 +0100

    Refactoring

--- src/app/modules/bemgular/directive.ts
+++ src/app/modules/bemgular/directive.ts
@@ -6,22 +6,13 @@ import { BEMGULAR_BLOCK, BEMGULAR_MODIFIERS } from './tokens';
 })
 export class BemgularDirective implements OnInit {
   private _EMPTY_STRING: string = '';
-  private _block: string;
-  private _modifiers: string[];
-  private _parentBlock: string;
-  private _parentModifiers: string[];
   private _value: string = '';
 
   constructor(
     @Inject(Injector) private _injector: Injector,
     @SkipSelf() private _parentInjector: Injector,
     private _elRef: ElementRef
-  ) {
-    this._block = this._injector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
-    this._modifiers = this._injector.get(BEMGULAR_MODIFIERS, []);
-    this._parentBlock = this._parentInjector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
-    this._parentModifiers = this._parentInjector.get(BEMGULAR_MODIFIERS, []);
-  }
+  ) {}
 
   @Input('bem')
   set storeElementAndModifiers(value: string) {
@@ -31,14 +22,18 @@ export class BemgularDirective implements OnInit {
     let split = value.split(',');
     let element: string = (!!split[0]) ? split[0].trim() : this._EMPTY_STRING;
     let elementModifiers: string[] = split.slice(1).map(modifier => modifier.trim());
-    if (this._block !== this._parentBlock) {
-      this._block = this._parentBlock;
-      this._modifiers = this._parentModifiers;
+
+    let block: string = this._injector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
+    let modifiers: string[] = this._injector.get(BEMGULAR_MODIFIERS, []);
+    let parentBlock: string = this._parentInjector.get(BEMGULAR_BLOCK, this._EMPTY_STRING);
+    if (block !== parentBlock) {
+      block = parentBlock;
+      modifiers = this._parentInjector.get(BEMGULAR_MODIFIERS, []);
     }
-    if (this._block === this._EMPTY_STRING) {
+    if (block === this._EMPTY_STRING) {
       return
     }
-    this._value = this.bemBlockArray(this._block, this._modifiers).map(blockWithModifiers => {
+    this._value = this.bemBlockArray(block, modifiers).map(blockWithModifiers => {
       return this.bemBlockArray(element, elementModifiers).map(elementWithModifiers => {
         return blockWithModifiers + '__' + elementWithModifiers;
       }).join(' ');
